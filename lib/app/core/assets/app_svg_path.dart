07ff7ed6016743cb0e7703241df20ee4a6f78e00
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-28 16:53:34 -0300

    feat: svg's and new dependencies

---
+++ lib/app/core/assets/app_svg_path.dart
@@ -0,0 +1,5 @@
+class AppSvgPath {
+  static String get arrowBack => "assets/svg/arrow_back.svg";
+  static String get warning => "assets/svg/warning.svg";
+  static String get search => "assets/svg/search.svg";
+}
