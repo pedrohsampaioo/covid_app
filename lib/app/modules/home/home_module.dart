6e5960ebb704766324d86bbc05de3c706d0523b4
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-30 16:16:45 -0300

    feat: home module dependency inject

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -3,8 +3,12 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../core/config/api_config.dart';
 import 'cubits/country/country_cubit.dart';
+import 'cubits/historic/historic_cubit.dart';
+import 'cubits/summary/summary_cubit.dart';
 import 'home_page.dart';
 import 'repositories/country_repository.dart';
+import 'repositories/historic_repository.dart';
+import 'repositories/summary_repository.dart';
 
 class HomeModule extends ChildModule {
   @override
@@ -12,6 +16,16 @@ class HomeModule extends ChildModule {
         Bind((i) => Dio(ApiConfig())),
         Bind((i) => CountryRepository(i.get<Dio>())),
         Bind((i) => CountryCubit(i.get<CountryRepository>())),
+        Bind((i) => SummaryRepository(i.get<Dio>())),
+        Bind((i) => HistoricRepository(i.get<Dio>())),
+        Bind(
+          (i) => SummaryCubit(i.get<SummaryRepository>()),
+          singleton: false,
+        ),
+        Bind(
+          (i) => HistoricCubit(i.get<HistoricRepository>()),
+          singleton: false,
+        ),
       ];
 
   @override
