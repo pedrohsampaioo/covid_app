ecee099ad2b010b935fad5734f5612b9db0ed5a9
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-30 16:13:26 -0300

    feat:  planet svg asset

--- lib/app/core/assets/app_svg_path.dart
+++ lib/app/core/assets/app_svg_path.dart
@@ -2,4 +2,5 @@ class AppSvgPath {
   static String get arrowBack => "assets/svg/arrow_back.svg";
   static String get warning => "assets/svg/warning.svg";
   static String get search => "assets/svg/search.svg";
+  static String get planet => "assets/svg/planet.svg";
 }
